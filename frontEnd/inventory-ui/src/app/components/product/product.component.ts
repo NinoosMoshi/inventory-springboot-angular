305a0327520c971507080bec534af069cd0361dd
NinoosMoshi <nalkaton@gmail.com>
2024-12-18 09:02:32 -0600

    fetch products

--- frontEnd/inventory-ui/src/app/components/product/product.component.ts
+++ frontEnd/inventory-ui/src/app/components/product/product.component.ts
@@ -1,12 +1,89 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { PaginationComponent } from '../pagination/pagination.component';
+import { ApiService } from '../../service/api.service';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, PaginationComponent],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit {
+
+  constructor(private apiService: ApiService, private router:Router) { }
+
+  products: any[] = [];
+  message: string = '';
+  currentPage: number = 1;
+  totalPages: number = 0;
+  itemsPerPage: number = 10;
+
+  ngOnInit(): void {
+    this.fetchProducts();
+    console.log("products: ",this.products)
+  }
+
+  fetchProducts() {
+    this.apiService.getAllProducts().subscribe({
+      next: (res) => {
+        const products = res.product || [];
+        this.totalPages = Math.ceil(products.length / this.itemsPerPage);
+        this.products = products.slice(
+          (this.currentPage -1 ) * this.itemsPerPage, 
+          this.currentPage * this.itemsPerPage
+        );
+        console.log("response: ",res)
+      },
+      error: (error) => {
+        console.log(error)
+        this.showMessage(error?.error?.message || error?.message || "Unable to fetch products" + error)
+      }
+    })
+  }
+
+
+  // HANDLE DELETE PRODUCT
+  handleProductDelete(productId: string): void {
+    if(window.confirm("Are you sure you want to delete this product?")){
+      this.apiService.deleteProduct(productId).subscribe({
+        next: (res) => {
+          if(res.status === 200) {
+            this.showMessage("Product deleted successfully");
+            this.fetchProducts();
+          }
+        },
+        error: (error) => {
+          this.showMessage(error?.error?.message || error?.message || "Unable to delete product" + error)
+        }
+      });
+    }
+  }
+
+
+  //HANDLE PAGE CHANGRTE. NAVIGATR TO NEXT< PREVIOUS OR SPECIFIC PAGE CHANGE
+  onPageChange(page: number): void {
+    this.currentPage = page;
+    this.fetchProducts();
+  }
+
+  //NAVIGATE TO ADD PRODUCT PAGE
+  navigateToAddProductPage(): void {
+    this.router.navigate(['/add-product']);
+  }
+
+  //NAVIGATE TO EDIT PRODUCT PAGE
+  navigateToEditProductPage(productId: string): void {
+    this.router.navigate([`/edit-product/${productId}`]);
+  }
+
+  showMessage(message:string){
+    this.message = message;
+    setTimeout(() =>{
+      this.message = ''
+    }, 4000)
+  }
 
 }
--- frontEnd/inventory-ui/src/app/components/supplier/supplier.component.ts
+++ frontEnd/inventory-ui/src/app/components/supplier/supplier.component.ts
@@ -1,12 +1,76 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../service/api.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-supplier',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './supplier.component.html',
   styleUrl: './supplier.component.css'
 })
-export class SupplierComponent {
+export class SupplierComponent implements OnInit {
+
+  suppliers:any[] = [];
+  message:string = '';
+
+  constructor(private apiService:ApiService, private router:Router) { }
+
+
+  ngOnInit(): void {
+    this.getSuppliers(); 
+  }
+
+  getSuppliers(){
+    this.apiService.getAllSuppliers().subscribe({
+      next:(res:any) => {
+        if(res.status == 200){
+          this.suppliers = res.suppliers;
+        }else {
+          this.showMessage(res.message);
+        }
+        
+      },
+      error:(error:any) => {
+        this.showMessage(error?.error?.message || error?.message || "Unable to get suppliers" + error)
+      }
+    })
+}
+
+
+// Navigate to add supplier page
+navigateToAddSupplierPage() : void {
+  this.router.navigate([`/add-supplier`]);
+}
+
+
+// Navigate to edit supplier page
+navigateToEditSupplierPage(supplierId:string) : void {
+  this.router.navigate([`/edit-supplier/${supplierId}`]);
+}
+
+handleDeleteSupplier(supplierId:string) : void {
+  if(window.confirm("Are you sure you want to delete this supplier?")){
+    this.apiService.deleteSupplier(supplierId).subscribe({
+      next:(res:any) => {
+        if(res.status == 200){
+          this.showMessage("Supplier deleted successfully");
+          this.getSuppliers();
+        }
+      },
+      error:(error:any) => {
+        this.showMessage(error?.error?.message || error?.message || "Unable to delete supplier" + error)
+      }
+    })
+  }
+}
+
+showMessage(message:string){
+  this.message = message;
+  setTimeout(() =>{
+    this.message = ''
+  }, 4000)
+}
 
 }
